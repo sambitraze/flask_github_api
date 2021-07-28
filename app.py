86d7ba1a26af9c55e4ca7dcfae24154f54bb98e8
SAMBIT MAJHI <majhisambit2@gmail.com>
2021-07-28 11:27:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, request, g, session, redirect, url_for
 from flask import render_template_string, jsonify
 from flask_github import GitHub
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
+DATABASE_URI = 'sqlite:////tmp/github-flask.db'
+SECRET_KEY = 'development key'
 DEBUG = True
 
 GITHUB_CLIENT_ID = 'Iv1.387ade82abe70fd3'
@@ -13,36 +18,62 @@ app.config.from_object(__name__)
 # setup github-flask
 github = GitHub(app)
 
+# setup sqlalchemy
+engine = create_engine(app.config['DATABASE_URI'])
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         autoflush=False,
+                                         bind=engine))
+Base = declarative_base()
+Base.query = db_session.query_property()
 
-# @app.before_request
-# def before_request():
-#     g.user = None
-#     if 'user_id' in session:
-#         g.user = User.query.get(session['user_id'])
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
 
 
-# @app.after_request
-# def after_request(response):
-#     db_session.remove()
-#     return response
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    github_access_token = Column(String(255))
+    github_id = Column(Integer)
+    github_login = Column(String(255))
+
+    def __init__(self, github_access_token):
+        self.github_access_token = github_access_token
+
+
+@app.before_request
+def before_request():
+    g.user = None
+    if 'user_id' in session:
+        g.user = User.query.get(session['user_id'])
+
+
+@app.after_request
+def after_request(response):
+    db_session.remove()
+    return response
 
 
 @app.route('/')
 def index():
-    # if g.user:
-    #     t = 'Hello! %s <a href="{{ url_for("user") }}">Get user</a> ' \
-    #         '<a href="{{ url_for("repo") }}">Get repo</a> ' \
-    #         '<a href="{{ url_for("logout") }}">Logout</a>'
-    #     t %= g.user.github_login
-    # else:
-    t = 'Hello! <a href="{{ url_for("login") }}">Login</a>'
+    if g.user:
+        t = 'Hello! %s <a href="{{ url_for("user") }}">Get user</a> ' \
+            '<a href="{{ url_for("repo") }}">Get repo</a> ' \
+            '<a href="{{ url_for("logout") }}">Logout</a>'
+        t %= g.user.github_login
+    else:
+        t = 'Hello! <a href="{{ url_for("login") }}">Login</a>'
 
     return render_template_string(t)
 
 
-# @github.access_token_getter
-# def token_getter():
-#     return g.user.github_access_token
+@github.access_token_getter
+def token_getter():
+    user = g.user
+    if user is not None:
+        return user.github_access_token
 
 
 @app.route('/github-callback')
@@ -52,23 +83,23 @@ def authorized(access_token):
     if access_token is None:
         return redirect(next_url)
 
-    # user = User.query.filter_by(github_access_token=access_token).first()
-    # if user is None:
-    #     user = User(access_token)
-    #     db_session.add(user)
+    user = User.query.filter_by(github_access_token=access_token).first()
+    if user is None:
+        user = User(access_token)
+        db_session.add(user)
 
-    # user.github_access_token = access_token
+    user.github_access_token = access_token
 
-    # # Not necessary to get these details here
-    # # but it helps humans to identify users easily.
-    # g.user = user
-    # github_user = github.get('/user')
-    # user.github_id = github_user['id']
-    # user.github_login = github_user['login']
+    # Not necessary to get these details here
+    # but it helps humans to identify users easily.
+    g.user = user
+    github_user = github.get('/user')
+    user.github_id = github_user['id']
+    user.github_login = github_user['login']
 
-    # db_session.commit()
+    db_session.commit()
 
-    # session['user_id'] = user.id
+    session['user_id'] = user.id
     return redirect(next_url)
 
 
